6d3331c4b83b48987387822f0001d6310e512924
Roman <76904779+Mr-R0m4n@users.noreply.github.com>
2022-03-12 22:52:42 +0100

    Update Canvas.js

--- src/components/Charts/Canvas.js
+++ src/components/Charts/Canvas.js
@@ -72,53 +72,61 @@ const Canvas = (props) => {
             const canvas = canvasRef.current;
             const context = canvas.getContext('2d');
 
+            context.clearRect(0, 0, canvas.width, canvas.height);
 
-            const GRAPH_TOP = 0;
-            const GRAPH_BOTTOM = canvas.height;
-            const GRAPH_LEFT = 0;
-            const GRAPH_RIGHT = canvas.width;
+            const GRAPH_CENTER_X = canvas.width / 2;
+            const GRAPH_CENTER_Y = canvas.height / 2;
+            const PIE_DIAMETER = canvas.width / 4;
 
-            const GRAPH_HEIGHT = GRAPH_BOTTOM - GRAPH_TOP;
-            const GRAPH_WIDTH = GRAPH_RIGHT - GRAPH_LEFT;
-
-            const GRAPH_CENTER_X = GRAPH_WIDTH / 2 + GRAPH_LEFT;
-            const GRAPH_CENTER_Y = GRAPH_HEIGHT / 2 + GRAPH_TOP;
-
-            const PIE_DIAMETER = GRAPH_WIDTH / 3.5;
-
-            context.clearRect(0, 0, GRAPH_WIDTH, GRAPH_HEIGHT);
-
-            const valueArray = state.values.filter(value => {
+            const values = state.values.filter(value => {
                 if (!value.isEmpty) {
                     return value;
                 } else {
                     return null;
                 }
             });
+            let sum = values.reduce((previous, current) => +previous + +current, 0);
 
-            const colours = ['#ffff00', '#80ff00', '#00ffbf', '#00bfff', '#8000ff']
-
+            const colours = ['#ff4000', '#ffbf00', '#bfff00', '#80ff00', '#00ff40', '#00ffff', '#0080ff', '#0040ff', '#8000ff', '#ff00ff'];
+            const PI2 = (2 * Math.PI);
             let startAngle = 0;
             let endAngle = 0;
-            let colourIndex = 0
-            let sum = valueArray.reduce((previous, current) => +previous + +current, 0);
-            valueArray.map(value => {
-                startAngle = endAngle
-                endAngle = endAngle + (Math.PI * 2 * (valueArray.length / sum));
-                context.fillStyle = colours[colourIndex++];
-                context.arc(GRAPH_CENTER_X, GRAPH_CENTER_Y, PIE_DIAMETER, startAngle, endAngle)
-                // console.log(startAngle)
-                console.log(endAngle)
-                context.lineTo(GRAPH_CENTER_X, GRAPH_CENTER_Y)
-                context.closePath();
-                context.fill();
-            })
+            let labelXPos
+            let labelYPos
+            let label;
 
-            // context.arc(GRAPH_WIDTH / 2 + GRAPH_LEFT, GRAPH_HEIGHT / 2 + GRAPH_TOP, (GRAPH_WIDTH / 3.5), 0, 360 * (Math.PI / 180));
-            // context.fillStyle = '#fff';
-            // context.lineTo(GRAPH_WIDTH / 2 + GRAPH_LEFT, GRAPH_HEIGHT / 2 + GRAPH_TOP);
-            // context.closePath();
-            // context.fill();
+            for (let i = 0; i < values.length; i++) {
+                context.beginPath();
+                context.moveTo(GRAPH_CENTER_X, GRAPH_CENTER_Y);
+
+                startAngle = endAngle;
+                endAngle = startAngle + (values[i] / sum) * PI2;
+                context.arc(GRAPH_CENTER_X, GRAPH_CENTER_Y, PIE_DIAMETER, startAngle, endAngle);
+
+                context.lineTo(GRAPH_CENTER_X, GRAPH_CENTER_Y);
+                if(i <= 0) {
+                    context.closePath();
+                }
+
+                context.lineWidth = 5;
+                context.stroke();
+                context.fillStyle = colours[i];
+                context.fill();
+
+                labelXPos = GRAPH_CENTER_X + PIE_DIAMETER * 1.3 * Math.cos(startAngle + Math.PI * values[i] / sum)
+                labelYPos = GRAPH_CENTER_Y + PIE_DIAMETER * 1.3 * Math.sin(startAngle + Math.PI * values[i] / sum)
+                label = state.titles[i];
+                context.font = "bold 20px Arial";
+                context.fillStyle = "#000";
+                context.fillText(label, labelXPos, labelYPos)
+
+                labelXPos = GRAPH_CENTER_X + PIE_DIAMETER * 0.8 * Math.cos(startAngle + Math.PI * values[i] / sum)
+                labelYPos = GRAPH_CENTER_Y + PIE_DIAMETER * 0.8 * Math.sin(startAngle + Math.PI * values[i] / sum)
+                label = `${Math.round(values[i]/sum * 100)}%`;
+                context.font = "bold 20px Arial";
+                context.fillStyle = "#fff";
+                context.fillText(label, labelXPos, labelYPos)
+            }
         }
     }, [canvas, props.chart, state]);
 
