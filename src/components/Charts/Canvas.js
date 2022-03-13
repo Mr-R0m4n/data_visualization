5940b3fea46f997d10fe0a5541a4b6197446d0b4
Roman <76904779+Mr-R0m4n@users.noreply.github.com>
2022-03-13 23:27:39 +0100

    Balkendiagramm hinzugefügt

--- src/components/Charts/Canvas.js
+++ src/components/Charts/Canvas.js
@@ -12,12 +12,7 @@ const Canvas = (props) => {
     const canvasRef = useRef();
     const canvas = canvasRef.current;
 
-    useEffect(() => {
-        setWidth(ref.current.clientWidth);
-        setHeight(ref.current.clientHeight);
-    }, []);
-
-    useEffect(() => {
+    const drawCoordinateSystem = () => {
         const canvas = canvasRef.current;
         const context = canvas.getContext('2d');
 
@@ -28,44 +23,45 @@ const Canvas = (props) => {
 
         const GRAPH_HEIGHT = GRAPH_BOTTOM - GRAPH_TOP;
 
-        if (props.chart !== 'pie') {
-            // draw X and Y axis
-            context.strokeStyle = "#FFF";
-            context.lineWidth = 2;
-            context.beginPath();
-            context.moveTo(GRAPH_RIGHT, GRAPH_BOTTOM);
-            context.lineTo(GRAPH_LEFT, GRAPH_BOTTOM);
-            context.lineTo(GRAPH_LEFT, GRAPH_TOP);
-            context.stroke();
+        // draw X and Y axis
+        context.strokeStyle = "#FFF";
+        context.lineWidth = 2;
+        context.beginPath();
+        context.moveTo(GRAPH_RIGHT, GRAPH_BOTTOM);
+        context.lineTo(GRAPH_LEFT, GRAPH_BOTTOM);
+        context.lineTo(GRAPH_LEFT, GRAPH_TOP);
+        context.stroke();
 
-            // draw reference line at the top of the graph
-            context.beginPath();
-            // set light grey color for reference lines
-            // context.strokeStyle = "#BBB";
-            context.lineWidth = 0.5;
-            context.moveTo(GRAPH_LEFT, GRAPH_TOP);
-            context.lineTo(GRAPH_RIGHT, GRAPH_TOP);
-            context.stroke();
+        // draw reference line at the top of the graph
+        context.beginPath();
+        context.lineWidth = 0.5;
+        context.moveTo(GRAPH_LEFT, GRAPH_TOP);
+        context.lineTo(GRAPH_RIGHT, GRAPH_TOP);
+        context.stroke();
 
-            // draw reference line 3/4 up from the bottom of the graph
-            context.beginPath();
-            context.moveTo(GRAPH_LEFT, (GRAPH_HEIGHT) / 4 * 3 + GRAPH_TOP);
-            context.lineTo(GRAPH_RIGHT, (GRAPH_HEIGHT) / 4 * 3 + GRAPH_TOP);
-            context.stroke();
+        // draw reference line 3/4 up from the bottom of the graph
+        context.beginPath();
+        context.moveTo(GRAPH_LEFT, (GRAPH_HEIGHT) / 4 * 3 + GRAPH_TOP);
+        context.lineTo(GRAPH_RIGHT, (GRAPH_HEIGHT) / 4 * 3 + GRAPH_TOP);
+        context.stroke();
 
-            // draw reference line 1/2 way up the graph
-            context.beginPath();
-            context.moveTo(GRAPH_LEFT, (GRAPH_HEIGHT) / 2 + GRAPH_TOP);
-            context.lineTo(GRAPH_RIGHT, (GRAPH_HEIGHT) / 2 + GRAPH_TOP);
-            context.stroke();
+        // draw reference line 1/2 way up the graph
+        context.beginPath();
+        context.moveTo(GRAPH_LEFT, (GRAPH_HEIGHT) / 2 + GRAPH_TOP);
+        context.lineTo(GRAPH_RIGHT, (GRAPH_HEIGHT) / 2 + GRAPH_TOP);
+        context.stroke();
 
-            // draw reference line 1/4 up from the bottom of the graph
-            context.beginPath();
-            context.moveTo(GRAPH_LEFT, (GRAPH_HEIGHT) / 4 + GRAPH_TOP);
-            context.lineTo(GRAPH_RIGHT, (GRAPH_HEIGHT) / 4 + GRAPH_TOP);
-            context.stroke();
-        }
-    }, [canvas, canvasRef, props.chart, state]);
+        // draw reference line 1/4 up from the bottom of the graph
+        context.beginPath();
+        context.moveTo(GRAPH_LEFT, (GRAPH_HEIGHT) / 4 + GRAPH_TOP);
+        context.lineTo(GRAPH_RIGHT, (GRAPH_HEIGHT) / 4 + GRAPH_TOP);
+        context.stroke();
+    }
+
+    useEffect(() => {
+        setWidth(ref.current.clientWidth);
+        setHeight(ref.current.clientHeight);
+    }, []);
 
     useEffect(() => {
         if (props.chart === 'pie') {
@@ -85,14 +81,14 @@ const Canvas = (props) => {
                     return null;
                 }
             });
-            let sum = values.reduce((previous, current) => +previous + +current, 0);
+           let sum = values.reduce((previous, current) => +previous + +current, 0);
 
             const colours = ['#ff4000', '#ffbf00', '#bfff00', '#80ff00', '#00ff40', '#00ffff', '#0080ff', '#0040ff', '#8000ff', '#ff00ff'];
             const PI2 = (2 * Math.PI);
             let startAngle = 0;
             let endAngle = 0;
-            let labelXPos
-            let labelYPos
+            let labelXPos;
+            let labelYPos;
             let label;
 
             for (let i = 0; i < values.length; i++) {
@@ -104,29 +100,118 @@ const Canvas = (props) => {
                 context.arc(GRAPH_CENTER_X, GRAPH_CENTER_Y, PIE_DIAMETER, startAngle, endAngle);
 
                 context.lineTo(GRAPH_CENTER_X, GRAPH_CENTER_Y);
-                if(i <= 0) {
+                if (i <= 0) {
                     context.closePath();
                 }
 
-                context.lineWidth = 5;
+                context.lineWidth = 10;
+                context.strokeStyle = '#73BD75';
                 context.stroke();
                 context.fillStyle = colours[i];
                 context.fill();
 
-                labelXPos = GRAPH_CENTER_X + PIE_DIAMETER * 1.3 * Math.cos(startAngle + Math.PI * values[i] / sum)
-                labelYPos = GRAPH_CENTER_Y + PIE_DIAMETER * 1.3 * Math.sin(startAngle + Math.PI * values[i] / sum)
-                label = state.titles[i];
-                context.font = "bold 20px Arial";
-                context.fillStyle = "#000";
-                context.fillText(label, labelXPos, labelYPos)
+                context.beginPath();
+                context.arc(GRAPH_CENTER_X, GRAPH_CENTER_Y, PIE_DIAMETER / 3, 0, PI2);
+                context.fillStyle = '#73BD75';
+                context.fill();
+                context.lineWidth = 3;
+                context.stroke();
+                context.closePath();
 
-                labelXPos = GRAPH_CENTER_X + PIE_DIAMETER * 0.8 * Math.cos(startAngle + Math.PI * values[i] / sum)
-                labelYPos = GRAPH_CENTER_Y + PIE_DIAMETER * 0.8 * Math.sin(startAngle + Math.PI * values[i] / sum)
-                label = `${Math.round(values[i]/sum * 100)}%`;
+                labelXPos = GRAPH_CENTER_X + PIE_DIAMETER * 1.2 * Math.cos(startAngle + Math.PI * values[i] / sum);
+                labelYPos = GRAPH_CENTER_Y + PIE_DIAMETER * 1.2 * Math.sin(startAngle + Math.PI * values[i] / sum);
+                label = state.titles[i];
+                context.textAlign = 'center';
+                context.textBaseline = 'middle';
+                context.font = "bold 20px";
+                context.fillStyle = "#000";
+                context.fillText(label, labelXPos, labelYPos);
+
+                labelXPos = GRAPH_CENTER_X + PIE_DIAMETER * 0.8 * Math.cos(startAngle + Math.PI * values[i] / sum);
+                labelYPos = GRAPH_CENTER_Y + PIE_DIAMETER * 0.8 * Math.sin(startAngle + Math.PI * values[i] / sum);
+                label = `${Math.round(values[i] / sum * 100)}%`;
                 context.font = "bold 20px Arial";
                 context.fillStyle = "#fff";
-                context.fillText(label, labelXPos, labelYPos)
+                context.fillText(label, labelXPos, labelYPos);
             }
+        }
+    }, [canvas, props.chart, state]);
+
+    useEffect(() => {
+        if (props.chart === 'bar') {
+            const canvas = canvasRef.current;
+            const context = canvas.getContext('2d');
+
+            const GRAPH_TOP = 50;
+            const GRAPH_BOTTOM = canvas.height - 100;
+            const GRAPH_LEFT = 100;
+            const GRAPH_RIGHT = canvas.width - 50;
+
+            const GRAPH_HEIGHT = GRAPH_BOTTOM - GRAPH_TOP;
+
+            context.clearRect(0, 0, canvas.width, canvas.height);
+            drawCoordinateSystem();
+
+            const values = state.values.filter(value => {
+                if (!value.isEmpty) {
+                    return +value;
+                } else {
+                    return null;
+                }
+            }).sort( function(a , b) { return a - b; } );
+
+            if(values.length > 0){
+
+                let label = `${values[values.length-1]}`;
+                context.textAlign = 'right';
+                context.textBaseline = 'middle';
+                context.font = "bold 20px Arial";
+                context.fillStyle = "#fff";
+                context.fillText(label, GRAPH_RIGHT, GRAPH_TOP - 15);
+
+                label = `${+(values[values.length-1])*3/4}`;
+                context.font = "bold 20px Arial";
+                context.fillStyle = "#fff";
+                context.fillText(label, GRAPH_RIGHT, GRAPH_TOP + GRAPH_HEIGHT / 4 - 15);
+
+                label = `${+(values[values.length-1])/2}`;
+                context.font = "bold 20px Arial";
+                context.fillStyle = "#fff";
+                context.fillText(label, GRAPH_RIGHT, GRAPH_TOP + GRAPH_HEIGHT / 2 - 15);
+
+                label = `${+(values[values.length-1])/4}`;
+                context.font = "bold 20px Arial";
+                context.fillStyle = "#fff";
+                context.fillText(label, GRAPH_RIGHT, GRAPH_TOP + GRAPH_HEIGHT * 3 / 4 - 15);
+
+                const colours = ['#ff4000', '#ffbf00', '#bfff00', '#80ff00', '#00ff40', '#00ffff', '#0080ff', '#0040ff', '#8000ff', '#ff00ff'];
+
+                context.beginPath()
+                context.rect(GRAPH_LEFT + 50, GRAPH_BOTTOM, 50, -GRAPH_HEIGHT)
+                context.fillStyle = colours[0];
+                context.fill();
+                context.closePath();
+            }
+        }
+    }, [canvas, props.chart, state]);
+
+    useEffect(() => {
+        if (props.chart === 'line') {
+            const canvas = canvasRef.current;
+            const context = canvas.getContext('2d');
+
+            context.clearRect(0, 0, canvas.width, canvas.height);
+            drawCoordinateSystem();
+
+            const values = state.values.filter(value => {
+                if (!value.isEmpty) {
+                    return value;
+                } else {
+                    return null;
+                }
+            });
+
+            const colours = ['#ff4000', '#ffbf00', '#bfff00', '#80ff00', '#00ff40', '#00ffff', '#0080ff', '#0040ff', '#8000ff', '#ff00ff'];
         }
     }, [canvas, props.chart, state]);
 
